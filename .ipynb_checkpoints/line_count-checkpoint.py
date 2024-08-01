06a74bf35330fe54296f79fd7f92b7134498498f
Tanu Khanuja <tanuk421@gmail.com>
2024-08-01 18:29:19 +0900

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/line_count-checkpoint.py
+++
@@ -1,9 +0,0 @@
-#line_count.py
-
-import sys
-
-count = 0
-for line in sys.stdin:
-    count += 1
-
-print(count)                    #by default print goes to sys.stdout, therefore it writes in console
